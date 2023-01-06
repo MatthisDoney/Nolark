2aca5357198db5800b36778d66271968ae06e89b
Matthis DONEY <matthis.doney@outlook.fr>
2023-01-06 10:59:02 +0100

    prise en charge email dans le formulaire

--- js/nolark.js
+++ js/nolark.js
@@ -5,14 +5,16 @@
 
 
 function validEnvoi() {
- if (window.document.querySelector("#i_nom").value === "" &&
- window.document.querySelector("#i_prenom").value === "" ) {
- alert("Le nom ou le prénom doivent être remplis"); // On affiche un message
- }
- else {
- window.document.querySelector("#form_contact").button(); // On peut envoyer
- }
+    if (window.document.querySelector("#i_nom").value === "" &&
+            window.document.querySelector("#i_prenom").value === "") {
+        alert("Le nom ou le prénom doivent être remplis"); // On affiche un message
+
+    } else if (window.document.querySelector("#i_email").value === "") {
+        alert("L'email doit être rempli"); // On affiche un message
+    } else {
+        window.document.querySelector("#form_contact").button(); // On peut envoyer
+    }
 }
-window.addEventListener("load", function() {
- window.document.querySelector("#btn_envoyer").addEventListener("click", validEnvoi);
+window.addEventListener("load", function () {
+    window.document.querySelector("#btn_envoyer").addEventListener("click", validEnvoi);
 });
