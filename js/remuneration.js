107582f994292177200350b84ca3313bd4da2053
Matthis DONEY <matthis.doney@outlook.fr>
2023-01-14 09:59:09 +0100

    Formulaire de remuneration terminé graphiquement mais pas fonctionnel

--- js/remuneration.js
+++ js/remuneration.js
@@ -4,6 +4,25 @@
  */
 
 
-function remuneration() {
+/*function remuneration() {
+    const fixe = 1100;
+    if ()
+}*/
+
+function comS20(nb) {
+const prixS20 = 140;
+const AxcomS20 = 0.02;
+return nb*prixS20*AxcomS20;
+
+}
+function ComMultitec(nb) {
+    const prixMult = 180;
+    if (nb<20) {
+        const AxcomMult = 4/100;
+    }
+    else if (nb>21 && nb<=50){
+        
+    }
     
+    return nb*prixMult*AxcomMult;
 }
